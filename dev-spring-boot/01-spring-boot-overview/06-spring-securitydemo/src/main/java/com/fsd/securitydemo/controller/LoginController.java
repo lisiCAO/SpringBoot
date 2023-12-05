54e31aadca65b87022ae0abfefca037627df6794
曹励斯 <caolisi@caolisideimac.home>
2023-12-05 00:33:18 -0500

    user authenticated

--- dev-spring-boot/01-spring-boot-overview/06-spring-securitydemo/src/main/java/com/fsd/securitydemo/controller/LoginController.java
+++ dev-spring-boot/01-spring-boot-overview/06-spring-securitydemo/src/main/java/com/fsd/securitydemo/controller/LoginController.java
@@ -1,2 +1,12 @@
-package com.fsd.securitydemo.controller;public class LoginController {
+package com.fsd.securitydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "login-form";
+    }
 }
--- dev-spring-boot/01-spring-boot-overview/06-spring-securitydemo/src/main/java/com/fsd/securitydemo/controller/demoController.java
+++ dev-spring-boot/01-spring-boot-overview/06-spring-securitydemo/src/main/java/com/fsd/securitydemo/controller/demoController.java
@@ -1,2 +1,15 @@
-package com.fsd.securitydemo.controller;public class demoController {
+package com.fsd.securitydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class demoController {
+
+    @GetMapping("/")
+    public  String showHome(){
+        return"Home";
+    }
+
+
 }
