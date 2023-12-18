765401032e86b321bd6c4799ff3a767fd4d537a7
LisiCAO <caolisi@hotmail.com>
2023-12-18 09:31:29 -0500

    rename library, add gpt project

---
+++ dev-spring-boot/03-spring-gpt/src/main/java/com/fsd/javagpt/JavaGptApplication.java
@@ -0,0 +1,13 @@
+package com.fsd.javagpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGptApplication.class, args);
+	}
+
+}
---
+++ dev-spring-boot/03-spring-gpt/src/test/java/com/fsd/javagpt/JavaGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsd.javagpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
